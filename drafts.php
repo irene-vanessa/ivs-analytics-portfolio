eaf20f6407fec044c401c491d9c9d8f358b1e58d
irene-vanessa <reenessav@gmail.com>
2026-01-01 19:54:57 +0000

    Initial portfolio upload

---
+++ drafts.php
@@ -0,0 +1,50 @@
+<article class="post featured">
+								<header class="major">
+									
+									<h2><a href="#">Data Cleaning, Analysis and Visualization in Excel<br />
+									</a></h2>
+									<p>In this project, we cleaned, analyzed, and visualized sales data of bike stores across Europe in Excel.</p>
+								</header>
+								<a href="#" class="image main"><img src="images/tsk.jpg" alt="" /></a>
+								<ul class="actions special">
+									<li><a href="https://github.com/irene-vanessa/bike-sales-analysis-excel" class="button large">View  Project</a></li>
+								</ul>
+							</article>
+
+
+							!-- Posts -->
+							<section class="posts">
+
+									<header>
+										<h2><a href="#">Web Scraping & Data Engineering Project in Python<br />
+										</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/web_scraping.WEBP" alt="" /></a>
+									<p>A data collection project that demonstrates practical web scraping skills by extracting and structuring information about Africa's largest companies by revenue. The project showcases the complete data pipeline from raw HTML to analysis-ready datasets.</p>
+									<ul class="actions special">
+										<li><a href="" class="button">View Project</a></li>
+									</ul>
+								</article>
+								
+								<article>
+									<header>
+										
+										<h2><a href="#">Exploratory Analysis of Global Layoffs in SQL <br />
+										</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/pathos_inspiring_layoffs.JPG" alt="" /></a>
+									<p>This project demonstrates advanced SQL usage, including joins, CTEs, window functions, and conditional aggregations, to explore and analyze global layoff trends.</p>
+									<ul class="actions special">
+										<li><a href="" class="button">View Project</a></li>
+									</ul>
+								</article>
+
+								<a href="#" class="image fit"><img src="images/web_scraping.WEBP" alt="" /></a>
+
+
+								
+								
+								css: 4235 #footer > section.split.contact {
+  display: flex;
+  justify-content: space-between;
+}
